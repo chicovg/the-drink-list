4699eb339f3f5ee72f99973490c2260051c44859
Victor Guthrie <guthrievictor@gmail.com>
2021-12-19 09:45:04 -0500

    removing old files

--- test/cljs/the_beer_list/runners/doo.cljs
+++
@@ -1,7 +0,0 @@
-(ns runners.doo
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [the-drink-list.core-test]
-            [the-drink-list.views-test]))
-
-(doo-tests 'the-drink-list.core-test
-           'the-drink-list.views-test)
--- test/cljs/the_beer_list/runners/tests.cljs
+++
@@ -1,3 +0,0 @@
-(ns runners.tests
-  (:require [the-drink-list.core-test]
-            [the-drink-list.views-test]))
