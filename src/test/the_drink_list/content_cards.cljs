fc0c2f7a892bef8706be2e541730b93d67134008
Victor Guthrie <guthrievictor@gmail.com>
2023-07-03 22:01:43 -0400

    more stories moved over

--- src/test/the_drink_list/content_cards.cljs
+++ src/test/the_drink_list/content_cards.cljs
@@ -1,14 +1,49 @@
 (ns the-drink-list.content-cards
   (:require
    [devcards.core :as dc]
-   [the-drink-list.uix-components.app :as app]
-   [uix.core :refer [$]]))
+   [the-drink-list.uix-components.logged-at :as logged-at]
+   [uix.core :refer [$ defui]]))
 
 (declare drink-list)
 
 (dc/defcard
   drink-list
   "TODO")
+
+(defn- date-minus-millis
+  [millis]
+  (js/Date.
+   (- (js/Date.)
+      millis)))
+
+(def examples (map date-minus-millis [(* 1000 30) ; 30 seconds
+                                      (* 1000 60) ; 1 minute
+                                      (* 1000 60 25) ; 25 minutes
+                                      (* 1000 60 59) ; 59 minutes
+                                      (* 1000 60 60) ; 1 hour
+                                      (* 1000 60 60 7) ; 7 hours
+                                      (* 1000 60 60 24) ; 1 day
+                                      (* 1000 60 60 24 3) ; 3 days
+                                      (* 1000 60 60 24 13) ; 1 week
+                                      (* 1000 60 60 24 22) ; 3 weeks
+                                      (* 1000 60 60 24 30) ; 1 month
+                                      (* 1000 60 60 24 30 5) ; 5 months
+                                      (* 1000 60 60 24 365) ; 1 year
+                                      (* 1000 60 60 24 365 3) ; 3 years
+                                      ]))
+
+(defui logged-at-preview []
+  ($ :div
+     (for [example examples]
+       ($ :div {:key example}
+          ($ :p.is-size-7
+             ($ :strong "Time: ") (str example))
+          ($ logged-at/logged-at {:date example})))))
+
+(declare logged-at)
+
+(dc/defcard logged-at
+  logged-at-preview)
 
 (declare navbar)
 
--- src/test/the_drink_list/input_field_cards.cljs
+++ src/test/the_drink_list/input_field_cards.cljs
@@ -3,11 +3,15 @@
    [devcards.core :as dc]
    [the-drink-list.types.drink :as drink]
    [the-drink-list.uix-components.autocomplete :as autocomplete]
+   [the-drink-list.uix-components.select-tags-input :as select-tags-input]
+   [the-drink-list.uix-components.slider-input :as slider-input]
+   [the-drink-list.uix-components.text-input :as text-input]
+   [the-drink-list.uix-components.textarea-input :as textarea-input]
    [uix.core :refer [$ defui use-state]]))
 
 (declare autocomplete)
 
-(defui autocomplete-story
+(defui autocomplete-preview
   []
   (let [[value set-value!] (use-state nil)]
     ($ autocomplete/autocomplete {:id :makers
@@ -18,28 +22,65 @@
 
 (dc/defcard
   autocomplete
-  ($ autocomplete-story))
+  ($ autocomplete-preview))
+
+(defui select-tags-preview []
+  (let [[value set-value!] (use-state nil)]
+    ($ select-tags-input/select-tags-input {:id          :tags
+                                            :label       "Tags"
+                                            :on-change   set-value!
+                                            :options     ["hoppy" "citrusy" "smoky" "toasty"]
+                                            :placeholder "Add Tags"
+                                            :style       {:width 200}
+                                            :value       value})))
 
 (declare select-tags)
 
 (dc/defcard
   select-tags
-  "TODO")
+  ($ select-tags-preview))
+
+(defui slider-input-preview []
+  (let [[value set-value!] (use-state 3)]
+    ($ slider-input/slider-input {:id        :rating
+                                  :min       "1"
+                                  :max       "5"
+                                  :label     "Rating"
+                                  :on-change set-value!
+                                  :step      "0.1"
+                                  :value     value})))
 
 (declare slider-input)
 
 (dc/defcard
   slider-input
-  "TODO")
+  ($ slider-input-preview))
+
+(defui text-input-preview []
+  (let [[value set-value!] (use-state nil)]
+    ($ text-input/text-input {:id          :name
+                              :label       "Name"
+                              :placeholder "Enter a Name"
+                              :on-change   set-value!
+                              :required    true
+                              :value       value})))
 
 (declare text-input)
 
 (dc/defcard
   text-input
-  "TODO")
+  ($ text-input-preview))
+
+(defui textarea-input-preview []
+  (let [[value set-value!] (use-state nil)]
+    ($ textarea-input/textarea-input {:id          :comment
+                                      :label       "Comment"
+                                      :placeholder "Enter a comment"
+                                      :on-change   set-value!
+                                      :value       value})))
 
 (declare textarea-input)
 
 (dc/defcard
   textarea-input
-  "TODO")
+  ($ textarea-input-preview))
